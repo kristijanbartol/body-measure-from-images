575a1ac2ed15800cbc41b7452ce14fe8c9b532df
kristijanbartol <kristijan.bartol@gmail.com>
2022-07-06 16:47:25 +0200

    Finish body measurements class.

--- model.py
+++ model.py
@@ -6,17 +6,15 @@ SMPL_NUM_KPTS = 23
 SMPLX_NUM_KPTS = 21
 
 MODEL_FILES_DIR = './model_files/'
-SMPL_DIR = os.path.join(MODEL_FILES_DIR, 'smpl')
-STAR_DIR = os.path.join(MODEL_FILES_DIR, 'star')
 
 
 def set_shape(model, shape_coefs):
     # TODO: Implement STAR support.
-    if type(model) == smplx.star.STAR:
-        return model(pose=torch.zeros((1, 72), device='cpu'), betas=shape_coefs, trans=torch.zeros((1, 3), device='cpu'))
-    else:
-        shape_coefs = torch.tensor(shape_coefs, dtype=torch.float32)
-        return model(betas=shape_coefs, return_verts=True)
+    #if type(model) == smplx.star.STAR:
+    #    return model(pose=torch.zeros((1, 72), device='cpu'), betas=shape_coefs, trans=torch.zeros((1, 3), device='cpu'))
+    #else:
+    shape_coefs = torch.tensor(shape_coefs, dtype=torch.float32)
+    return model(betas=shape_coefs, return_verts=True)
 
 
 def create_model(gender, num_coefs=10, model_type='smpl'):
@@ -25,7 +23,7 @@ def create_model(gender, num_coefs=10, model_type='smpl'):
         return smplx.star.STAR()
     else:
         body_pose = torch.zeros((1, SMPL_NUM_KPTS * 3))
-        return smplx.create(SMPL_DIR, model_type=model_type,
+        return smplx.create(MODEL_FILES_DIR, model_type=model_type,
                             gender=gender, use_face_contour=False,
                             num_betas=num_coefs,
                             body_pose=body_pose,
